f6f30210162ad4f2236b71b6472858f193356dbb
Blake <bamartin1618@gmail.com>
2024-06-07 02:36:39 -0500

    Fix application launch and environment configurations

--- app.py
+++ app.py
@@ -136,4 +136,5 @@ with gr.Blocks() as app:
         outputs=result_text
     )
 
-app.launch()
+if __name__ == "__main__":
+    app.launch(server_name="0.0.0.0", server_port=os.environ.get('PORT', 7860), debug=False)
